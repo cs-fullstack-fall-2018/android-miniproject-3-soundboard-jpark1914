d32ee6de5f7cee8f416e470295167900e24f1ca0
Jordan Parker <parkerj4321@gmail.com>
2018-11-20 12:01:16 -0600

    Still working

--- app/src/main/java/org/code_crew/soundboard/MainActivity.java
+++ app/src/main/java/org/code_crew/soundboard/MainActivity.java
@@ -1,13 +1,46 @@
 package org.code_crew.soundboard;
 
+import android.media.AudioAttributes;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int sound1, sound2, sound3, sound4, sound5, sound6;
+    private SoundPool soundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build();
+
+        soundPool = new SoundPool.Builder()
+                .setMaxStreams(6)
+                .setAudioAttributes(audioAttributes)
+                .build();
+
+        sound1 = soundPool.load(this,R.raw.sound1,1);
+        sound2 = soundPool.load(this,R.raw.sound2,1);
+        sound3 = soundPool.load(this,R.raw.sound3, 1);
+        sound4 = soundPool.load(this, R.raw.sound4, 1);
+        sound5 = soundPool.load(this, R.raw.sound5, 1);
+
+
     }
+
+    public void soundOn(View view) {
+        switch (view.getId()){
+            case (R.id.sound1):
+
+
+        }
+    }
+
 }
